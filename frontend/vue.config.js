21a3b2e0465df88e47fe580b2d385c1b5dcf5f22
waridrox <mohdwarid4@gmail.com>
2023-01-19 20:59:55 +0530

    Updated vue version
    Signed-off-by: waridrox <mohdwarid4@gmail.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/digit-recognition'
-    : '/'
+    : '/',
+  configureWebpack: {
+    experiments: {
+      asyncWebAssembly: true
+    }
+  }
 }
